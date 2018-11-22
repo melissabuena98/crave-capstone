d87d9e4ba7d06a6cf1a9649a0b162ff054bdb2a9
MBuena <Mbuena@student.neumont.edu>
2018-11-21 21:32:23 -0700

    View Profile on Feed Post Click

--- front-end/crave.js
+++ front-end/crave.js
@@ -1073,7 +1073,7 @@ app.controller("ViewProfileController", function($scope, DashboardService, Profi
                 $scope.viewlocation = response.data.location;
                 $scope.viewbio = response.data.bio;
                 $scope.viewimagePreviewUrl = response.data.profile_pic;
-            })
+            });
         
             ProfileService.getUserPosts().then(function(response){
                 console.log("GET USER POSTS", response.data);
@@ -1108,6 +1108,39 @@ app.controller("ViewProfileController", function($scope, DashboardService, Profi
         else{
             $scope.followStatus = "Follow";
         }
+    }
+
+    $scope.viewPost = function(postIndex){
+        console.log("VIEW POST: ", postIndex);
+        $scope.viewPostID = $scope.posts[postIndex]._id;
+        $scope.viewImage = $scope.posts[postIndex].image;
+        $scope.viewAvatar = $scope.viewimagePreviewUrl;
+        $scope.viewUsername = $scope.posts[postIndex].username;
+        $scope.viewTitle = $scope.posts[postIndex].title;
+        $scope.viewLocation = $scope.posts[postIndex].location;
+        $scope.viewCaption = $scope.posts[postIndex].caption;
+        $scope.viewLikes = $scope.posts[postIndex].likes.length;
+        modal = document.getElementById('myModal');
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        modal.style.display = "block";
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }  
     }
 })
 
