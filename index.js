a7565517b0a13ee05cae3c34954bfa427e0b0c02
MBuena <Mbuena@student.neumont.edu>
2018-11-29 11:36:18 -0700

    fix port

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.get('/', function(req, res) {
     res.send("hello from server")
 })
 
-app.listen(PORT, function() {
-    console.log("server running on localhost: " + PORT);
+app.listen(process.env.PORT, function() {
+    console.log("server running on localhost: ");
 })
